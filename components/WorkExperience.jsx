5bed0c52671a9c182218138c6dc0b5d406c2efa3
Olanrewaju A. Olaboye <boyepanthera@gmail.com>
2025-05-20 08:27:43 -0500

    work experience

--- components/WorkExperience.jsx
+++ components/WorkExperience.jsx
@@ -13,35 +13,43 @@ export default function WorkExperience() {
       period: "Jun 2021 - Present",
       responsibilities: [
         "Spearheaded the development of a Learning Management System for 2,000+ users, using TypeScript, React, and MongoDB.",
-        "Led a team to create a geo-fencing and face recognition app that verifies user identity and location, used across Nigeria.",
+        "Led a team to create a geo-fencing and face recognition app that verifies user identity and location, used across Nigerian public sector.",
         "Developed a real-time tracking solution, AppointCare Connect, integrated with over 15 hospital banks, impacting over 20,000 banking agencies.",
       ],
     },
     {
       company: "Panthera Tech School",
-      role: "Lead Backend Developer",
-      period: "Jul 2019 - Jun 2021",
+      role: "Lead Bootcamp Instructor",
+      period: "Jan 2020 - December 2024",
       responsibilities: [
         "Designed and upgraded the curriculum for Node.js and React.js bootcamp, training over 3,000 students.",
         "Mentored graduate who became Software Backend, Full stack, and Mobile App development roles across Nigeria.",
       ],
     },
     {
-      company: "Edufit (Remote)",
+      company: "Edlyft (Remote)",
       role: "Lead Software Engineer",
-      period: "May 2017 - Jun 2020",
+      period: "May 2021 - Jun 2022",
       responsibilities: [
-        "Developed Web features for Edufit's LMS web app, increasing learner-student engagement and course completion.",
+        "Developed Web features for Edlyft's LMS web app, increasing learner-student engagement and course completion.",
         "Engineered a speech AI tech used by over 5,000 students, enhancing learning effectiveness.",
       ],
     },
     {
       company: "Guaranteed Trust Bank",
       role: "FinTech Technology Developer",
-      period: "Jan 2016 - Jun 2018",
+      period: "Jan 2019 - July 2020",
       responsibilities: [
         "Created web applications to automate banking processes using Python, Node.js, React, and SQL.",
         "Designed protocol module for loan management automation and expected credit loss (ECL) compliance.",
+      ],
+    },
+    {
+      company: "Successtrend IT Firm",
+      role: "February 2017 - October 2018",
+      responsibilities: [
+        "Contributed as a Junior Web Developer at SuccessTrend IT Firm, collaborating with senior developers to create SmartRecharge.ng and MobileOne.ng, turned key utilities payment portals for resellers and agents. These platforms processed over N100 million in annual sales in 2019 and 2020 ",
+        "Build frontend code with, html, css, JQuery, Js and Bootstrap used by the senior engineers as template engine UI components/pages.",
       ],
     },
   ];
